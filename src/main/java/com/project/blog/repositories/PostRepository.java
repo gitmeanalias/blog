efed73549047a8933176ac44329adc507a4175c9
gitmeanalias <givemeanalias@protonmail.com>
2021-02-25 15:54:21 -0300

    Add Post repository

---
+++ src/main/java/com/project/blog/repositories/PostRepository.java
@@ -0,0 +1,9 @@
+package com.project.blog.repositories;
+
+import com.project.blog.entities.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PostRepository extends JpaRepository<Post, Long> {
+}
