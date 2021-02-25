f1fff74288c341b5f70c2ee117f64ef6442238ce
gitmeanalias <givemeanalias@protonmail.com>
2021-02-25 15:22:30 -0300

    Update Comment entity

--- src/main/java/com/project/blog/entities/Comment.java
+++ src/main/java/com/project/blog/entities/Comment.java
@@ -1,2 +1,61 @@
-package com.project.blog.entities;public class Comment {
+package com.project.blog.entities;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "comments")
+public class Comment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(columnDefinition = "TEXT")
+    private String content;
+
+    @Column
+    private int likes;
+
+    @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @JoinColumn(name = "post_id", nullable = false)
+    private Post post;
+
+    public Comment() {
+        //
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public int getLikes() {
+        return likes;
+    }
+
+    public void setLikes(int likes) {
+        this.likes = likes;
+    }
+
+    @JsonIgnore
+    public Post getPost() {
+        return post;
+    }
+
+    @JsonIgnore
+    public void setPost(Post post) {
+        this.post = post;
+    }
+
+    public Long getPostId() {
+        return post.getId();
+    }
 }
