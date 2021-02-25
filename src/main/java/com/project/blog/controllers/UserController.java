0dcf2097f33e5444a9c0be3a45b76c5f00f678d9
gitmeanalias <givemeanalias@protonmail.com>
2021-02-25 17:18:20 -0300

    Add User rest controller

---
+++ src/main/java/com/project/blog/controllers/UserController.java
@@ -0,0 +1,29 @@
+package com.project.blog.controllers;
+
+import com.project.blog.entities.User;
+import com.project.blog.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController(value = "/api/v1/")
+public class UserController {
+    @Autowired private UserService usrService;
+
+    @GetMapping(path = "/users")
+    public ResponseEntity<List<User>> list() {
+        return ResponseEntity.ok(usrService.getUsers());
+    }
+
+    @GetMapping(path = "/users/{id}")
+    public ResponseEntity<User> listUser(@PathVariable(value = "id") Long id) {
+        return ResponseEntity.ok(usrService.getUserById(id).get());
+    }
+
+    @PostMapping(path = "/users")
+    public ResponseEntity<User> saveUser(@RequestBody User user) {
+        return ResponseEntity.ok(usrService.createUser(user));
+    }
+}
