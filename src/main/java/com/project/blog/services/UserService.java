f405eb5bebe5040d8635427825406ca85aa07054
gitmeanalias <givemeanalias@protonmail.com>
2021-02-25 16:07:38 -0300

    Add User service

--- src/main/java/com/project/blog/services/UserService.java
+++ src/main/java/com/project/blog/services/UserService.java
@@ -1,4 +1,31 @@
 package com.project.blog.services;
 
+import com.project.blog.entities.User;
+import com.project.blog.exceptions.ResourceNotFoundException;
+import com.project.blog.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class UserService {
+    @Autowired private UserRepository userRepo;
+
+    public List<User> getUsers() {
+        return userRepo.findAll();
+    }
+
+    public Optional<User> getUserById(Long id) {
+        if(!userRepo.existsById(id)) {
+            throw new ResourceNotFoundException("User with id "+id+" does not exist.");
+        }
+        return userRepo.findById(id);
+    }
+
+    public User createUser(User user) {
+        return userRepo.save(user);
+    }
+
 }
