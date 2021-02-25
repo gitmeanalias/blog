67420cb8da9e30a1d6a95d2a5c8d78e8951f8509
gitmeanalias <givemeanalias@protonmail.com>
2021-02-25 15:57:02 -0300

    Add Reply repository

---
+++ src/main/java/com/project/blog/repositories/ReplyRepository.java
@@ -0,0 +1,9 @@
+package com.project.blog.repositories;
+
+import com.project.blog.entities.Reply;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ReplyRepository extends JpaRepository<Reply, Long> {
+}
