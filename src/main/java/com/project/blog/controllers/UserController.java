7ff51c4caad4726e8797c76b0e13fd4d5abd6b3e
gitmeanalias <givemeanalias@protonmail.com>
2021-02-25 17:45:50 -0300

    Update User rest controller

--- src/main/java/com/project/blog/controllers/UserController.java
+++ src/main/java/com/project/blog/controllers/UserController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RestController(value = "/api/v1/")
+@RestController()
 public class UserController {
     @Autowired private UserService usrService;
 
