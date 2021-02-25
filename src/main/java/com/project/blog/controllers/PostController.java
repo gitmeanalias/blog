44716cad3bd9208361ecdbfe00a4f2df8250c7e7
gitmeanalias <givemeanalias@protonmail.com>
2021-02-25 17:45:07 -0300

    Update Post rest controller

--- src/main/java/com/project/blog/controllers/PostController.java
+++ src/main/java/com/project/blog/controllers/PostController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RestController(value = "/api/v1/")
+@RestController()
 public class PostController {
     @Autowired private PostService postService;
 
